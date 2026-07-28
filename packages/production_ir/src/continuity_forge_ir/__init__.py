e76dc5a2eb7b8bf9debb046ea32696656aeae455
Daniel Meyer <scrimshawlife@gmail.com>
2026-07-28 10:34:55 -0700

    feat(ir): export Production IR models

---
+++ packages/production_ir/src/continuity_forge_ir/__init__.py
@@ -0,0 +1,21 @@
+from .models import (
+    AtomType,
+    CoverageStatus,
+    NarrativeAtom,
+    SceneNode,
+    ScriptDocument,
+    SourceSpan,
+    content_hash,
+    stable_id,
+)
+
+__all__ = [
+    "AtomType",
+    "CoverageStatus",
+    "NarrativeAtom",
+    "SceneNode",
+    "ScriptDocument",
+    "SourceSpan",
+    "content_hash",
+    "stable_id",
+]
